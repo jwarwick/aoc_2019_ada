344157b6c672a04347535457b79605d1c815154a
John Warwick <jwarwick@gmail.com>
2020-06-17 11:30:44 -0400

    Day 1, Part 1

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # 2019 Advent of Code
 
 [Advent of Code (AOC)](http://adventofcode.com/2019) exercises in Ada
+
+## How to Install Ada
+Not available in Homebrew, download this package: (https://www.adacore.com/download)
+
+## How to Build
+```
+% cd day01
+% gprbuild
+% ./obj/day1
+```
+
--- day01/day1.adb
+++ day01/day1.adb
@@ -3,12 +3,12 @@ with Ada.Text_IO;
 
 procedure day1 is
   package TIO renames Ada.Text_IO;
-  total : Integer := 0;
+  total : Natural := 0;
 
-  function fuel_required(mass : Positive) return Integer is
+  function fuel_required(mass : Natural) return Natural is
     req : Positive;
   begin
-    req := integer(Float'Floor(float(mass) / 3.0) - 2.0);
+    req := positive(Float'Floor(float(mass) / 3.0) - 2.0);
     return req;
   end fuel_required;
 
@@ -17,12 +17,12 @@ procedure day1 is
   begin
     TIO.open(File => file, Mode => TIO.In_File, Name => filename);
     while not TIO.end_of_file(file) Loop
-      total := total + fuel_required(Positive'Value(TIO.get_line(file)));
+      total := total + fuel_required(Natural'Value(TIO.get_line(file)));
     end loop;
     TIO.close(file);
   end sum_file;
 
 begin
   sum_file("day1_input.txt");
-  TIO.put_line("Total: " & Integer'Image(total));
+  TIO.put_line("Total: " & Natural'Image(total));
 end day1;
